09a493012a4fdcfcddf1d9fde975fe40d5627b34
小乌龟 <666666@qq.com>
2018-11-19 10:44:50 +0800

    vue.config.js配置注释

--- src/pages/index/views/About.vue
+++ src/pages/index/views/About.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="about">
-        <h1>This is an about page</h1>
+        <h1>This is an about page222222222222</h1>
+        <div id='main' style='width: 600px; height:400px;'></div>
+    
     </div>
 </template>
+<script>
+export default {
+  name: "",
+  components: {},
+  mounted() {
+    console.log(this.$echarts);
+    var myChart = this.$echarts.init(
+      document.getElementById("main"),
+      "macarons"
+    );
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: "ECharts 入门示例"
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"]
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    console.log();
+  }
+};
+</script>
+
